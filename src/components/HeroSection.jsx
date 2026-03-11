f75cd9101ba8cb4c8119f0735b007113ba630ddc
Mortenlost <musicaarhe@gmail.com>
2026-03-10 18:15:53 -0600

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+      <h1 className='text-4xl text-amber-700'> hola </h1>
+    </div>
+  )
+}
+
+export default HeroSection 
