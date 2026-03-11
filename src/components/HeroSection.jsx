ebe263659abc788eac701f38f4399868cd02dced
Mortenlost <musicaarhe@gmail.com>
2026-03-10 18:33:11 -0600

    Update models

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Vert3xDrone } from '../models/Vert3x-drone'
 
 const HeroSection = () => {
   return (
     <div>
       <h1 className='text-4xl text-amber-700'> hola </h1>
+      
     </div>
   )
 }
